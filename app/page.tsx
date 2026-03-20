dcbc56983420095a0b8007d66bd7cd26e360a136
mrbin509 <bineetkumarsahu771@gmail.com>
2026-03-21 01:08:34 +0530

    Tailwind fixed and working

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-import Hero from "@/components/Hero";
-import Stats from "@/components/Stats";
-import CoursesSection from "@/components/CoursesSection";
-import ReferSection from "@/components/ReferSection";
-import Features from "@/components/Features";
-import Testimonials from "@/components/Testimonials";
-import CTA from "@/components/CTA";
-import Footer from "@/components/Footer";
-
 export default function Home() {
   return (
-    <>
-      <Hero />
-      <Stats />
-      <CoursesSection />
-      <ReferSection />
-      <Features />
-      <Testimonials />
-      <CTA />
-      <Footer />
-    </>
+    <div className="bg-red-500 text-white text-3xl p-10">
+      Tailwind Working 🚀
+    </div>
   );
 }
